f31f9d2abafe69195458334ada81f125d55b4200
akuni <seinaruakuni@gmail.com>
2026-04-14 13:12:26 +0200

    commit test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,14 +3,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 
-Calculator("-(-((--(7,2*--3)**(2+--4)/((--(-3,5)**2+--(-1,7*--2)))+((--5,1**-(-3+2)-(-6,2**3))/((4**(2**3))-(-5,3*--2,1)**(2-1))))+(((--6,8*--1)**-(--2+3)/(((-3**2+--2,4)*(-1,3))**2))-(--(3,7+--2,1)**(2-(-1))+((--5,2*--4)**-(2+3)/((-4,1)**(3+1)+--(-2,3*--1))))))+(-(-((--2,3*--5)**(3+2)/((--(-1,5)**(2-1)+--(-3,2*--2)))+((--4,2**-(1+3)-(-(-6,1)**2))/((3**(2**2))-(-2,5*--1,3)**(3-2)))))))");
+Calculator("2**3**-5**5**-2**-4**6");
 //Console.WriteLine(RemoveBreckets("(((((2+2)))))"));
 static void Calculator(string input)
 {
     input = RemoveBreckets(input);
     //array ordered by priority operations
     string[] operations = { "(", ")", "", "", "*", "/", "+", "-" };
-
+      
     List < (int parent,
           int operation,
           int index,
@@ -109,8 +109,6 @@ static double CCreateSolvingTree(string input, string[] operations, List<(int pa
         }
         else
         {
-
-
 
             if (SolvingTree[indexToSolve].operation != 3)
             {
